73267634d57b9cc0aa965bbea8d33be2548eb02b
anth0nycodes <hoanganthony2207@gmail.com>
2025-09-24 22:49:36 -0400

    feat(resume): updated resume info

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -11,12 +11,13 @@ export const DATA = {
     "Software Engineer. Part-time LinkedIn warrior. I love building cool things.",
   summary:
     "Hi, my name is Anthony Hoang and I'm based in NYC. I like creating cool things and learning new things.",
-  avatarUrl: "/me.png",
+  avatarUrl: "/mepfpupdated.png",
   skills: [
     "Next.js",
     "React",
+    "React Native",
+    "tRPC",
     "TypeScript",
-    "Python",
     "Supabase",
     "Tailwind CSS",
     "Shadcn UI",
@@ -28,6 +29,7 @@ export const DATA = {
     "Git",
     "Vite",
     "Figma",
+    "Linear",
   ],
   navbar: [{ href: "/", icon: HomeIcon, label: "Home" }],
   contact: {
@@ -58,10 +60,18 @@ export const DATA = {
     {
       company: "Magic Hour (YC W24)",
       href: "https://magichour.ai",
+      title: "Software Engineer",
+      logoUrl: "/magichour.png",
+      start: "August 2025",
+      end: "September 2025",
+    },
+    {
+      company: "Magic Hour (YC W24)",
+      href: "https://magichour.ai",
       title: "Software Engineer Intern",
       logoUrl: "/magichour.png",
       start: "March 2025",
-      end: "Present",
+      end: "August 2025",
     },
     {
       company: "RevisionDojo (YC F24)",
@@ -87,7 +97,7 @@ export const DATA = {
       degree: "Bachelor's Degree of Computer Science (BS)",
       logoUrl: "/lehman.png",
       start: "2025",
-      end: "2029",
+      end: "Dropped Out 😎",
     },
     {
       school: "DeWitt Clinton High School",
@@ -99,31 +109,31 @@ export const DATA = {
     },
   ],
   projects: [
-    {
-      title: "Beacon",
-      href: "https://trybeacon.app",
-      dates: "May 2025 - Present",
-      description:
-        "An AI-powered study assistant that transforms your PDFs, documents, slides, and text into interactive revision sets.",
-      technologies: [
-        "Next.js",
-        "TypeScript",
-        "Tailwind CSS",
-        "Supabase",
-        "Drizzle ORM",
-        "Vercel AI SDK",
-        "Zod",
-        "Shadcn UI",
-      ],
-      links: [
-        {
-          type: "Website",
-          href: "https://trybeacon.app",
-          icon: <Icons.globe className="size-3" />,
-        },
-      ],
-      image: "/beacon.png",
-    },
+    // {
+    //   title: "Beacon",
+    //   href: "https://trybeacon.app",
+    //   dates: "May 2025 - Present",
+    //   description:
+    //     "An AI-powered study assistant that transforms your PDFs, documents, slides, and text into interactive revision sets.",
+    //   technologies: [
+    //     "Next.js",
+    //     "TypeScript",
+    //     "Tailwind CSS",
+    //     "Supabase",
+    //     "Drizzle ORM",
+    //     "Vercel AI SDK",
+    //     "Zod",
+    //     "Shadcn UI",
+    //   ],
+    //   links: [
+    //     {
+    //       type: "Website",
+    //       href: "https://trybeacon.app",
+    //       icon: <Icons.globe className="size-3" />,
+    //     },
+    //   ],
+    //   image: "/beacon.png",
+    // },
     {
       title: "AI Meme Generator",
       href: "https://magichour.ai/products/ai-meme-generator",
